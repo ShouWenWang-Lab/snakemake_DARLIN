8367951bf2db6d27ec3f319907e10f6c1c07c869
ShouWenWang <wangsw09@gmail.com>
2022-05-02 10:28:55 -0400

    Extend choices of CARLIN pipeline: including Tigre_2022_v2 etc

--- source/help_functions.py
+++ source/help_functions.py
@@ -27,14 +27,24 @@ def update_CARLIN_dir(CARLIN_root_folder, template):
             f"rsync -avP '{CARLIN_root_folder}/Custom_CARLIN/' '{CARLIN_root_folder}/Tigre_CARLIN_2022'"
         )
         Actual_CARLIN_dir = f"{CARLIN_root_folder}/Tigre_CARLIN_2022"
+    elif template == "Tigre_2022_v2":
+        os.system(
+            f"rsync -avP '{CARLIN_root_folder}/Custom_CARLIN/' '{CARLIN_root_folder}/Tigre_CARLIN_2022_v2'"
+        )
+        Actual_CARLIN_dir = f"{CARLIN_root_folder}/Tigre_CARLIN_2022_v2"
     elif template == "Rosa":
         os.system(
             f"rsync -avP '{CARLIN_root_folder}/Custom_CARLIN/' '{CARLIN_root_folder}/Rosa_CARLIN'"
         )
         Actual_CARLIN_dir = f"{CARLIN_root_folder}/Rosa_CARLIN"
+    elif template == "Rosa_v2":
+        os.system(
+            f"rsync -avP '{CARLIN_root_folder}/Custom_CARLIN/' '{CARLIN_root_folder}/Rosa_CARLIN_v2'"
+        )
+        Actual_CARLIN_dir = f"{CARLIN_root_folder}/Rosa_CARLIN_v2"
     else:
         raise ValueError(
-            "The input template should be among {Rosa, Tigre_2022, Tigre, cCARLIN}"
+            "The input template should be among {Rosa, Tigre_2022, Tigre, cCARLIN, Rosa_v2, Tigre_2022_v2}"
         )
     return Actual_CARLIN_dir
 
