20e94f02e85567153992f38eea3cb505e350dbcd
ShouWenWang <wangsw09@gmail.com>
2024-04-11 10:36:53 +0800

    Fix

--- test/test.sh
+++ test/test.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 cd bulk_cCARLIN
-# run CARLIN pipeline (based on matlab)
-snakemake -s ../../snakefiles/snakefile_integrate_CARLIN.py  --configfile config.yaml --core 10 --config sbatch=0 -R CARLIN
+# run matlab-based DARLIN pipeline 
+snakemake -s ../../snakefiles/snakefile_matlab_DARLIN_Part1.py  --configfile config.yaml --core 10 --config sbatch=0 -R DARLIN
+# merge results from all samples (we have only one sample 'test' here. So, there is not much difference)
+snakemake -s ../../snakefiles/snakefile_matlab_DARLIN_Part2.py --configfile config.yaml --core 5 --ri -R generate_report -R plots 
 
 cd ../sc-10X
-# run CARLIN pipeline (based on matlab)
-snakemake -s ../../snakefiles/snakefile_integrate_CARLIN.py  --configfile config.yaml --core 10 --config sbatch=0 -R CARLIN
+# run matlab-based DARLIN pipeline 
+snakemake -s ../../snakefiles/snakefile_matlab_DARLIN_Part1.py  --configfile config.yaml --core 10 --config sbatch=0 -R DARLIN
+
 # run customized python pipeline on single-cell data
-snakemake -s ../../snakefiles/snakefile_single_cell_DARLIN.py  --configfile config.yaml --core 5 --config sbatch=0 -R CARLIN
+snakemake -s ../../snakefiles/snakefile_python_DARLIN.py  --configfile config.yaml --core 5 --config sbatch=0 -R DARLIN
 
 
 cd ../scCamellia
-# run CARLIN pipeline (based on matlab)
-snakemake -s ../../snakefiles/snakefile_integrate_CARLIN.py  --configfile config.yaml --core 10 --config sbatch=0 -R CARLIN
+# run matlab-based DARLIN pipeline 
+snakemake -s ../../snakefiles/snakefile_matlab_DARLIN_Part1.py  --configfile config.yaml --core 10 --config sbatch=0 -R DARLIN
+
 # run customized python pipeline on single-cell data
-snakemake -s ../../snakefiles/snakefile_single_cell_DARLIN.py  --configfile config.yaml --core 5 --config sbatch=0 -R CARLIN
+snakemake -s ../../snakefiles/snakefile_python_DARLIN.py  --configfile config.yaml --core 5 --config sbatch=0 -R DARLIN
